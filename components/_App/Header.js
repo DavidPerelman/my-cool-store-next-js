35fba80a8e808dee7189a597840088c68aae21b1
David Perelman <dperelman3@gmail.com>
2023-02-07 10:26:35 +0200

    change header styling

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,7 +1,23 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import classes from './Header.module.css';
 
 const Header = ({ children }) => {
-  return <div className={classes.Header}></div>;
+  return (
+    <div>
+      {/* <header className={classes.Header}> */}
+      <Navbar className={classes.Header}>
+        <Container>
+          {/* <LinkContainer to='/'> */}
+          <Navbar.Brand className={classes['site-title']}>
+            MyCoolStore
+          </Navbar.Brand>
+          {/* </LinkContainer> */}
+        </Container>
+      </Navbar>
+      {/* </header> */}
+    </div>
+  );
 };
 
 export default Header;

82a8c3313409bca4605481f5735aecf73a69e758
David Perelman <dperelman3@gmail.com>
2023-02-09 10:43:33 +0200

    deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import '@/styles/globals.css';
+import '@/public/styles/globals.css';
 // import '@/styles/nprogress.css';
 config.autoAddCss = false;
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '/static/styles.css';
-import '/static/nprogress.css';
+import '/public/static/nprogress.css';
 
 import Layout from '@/components/_App/Layout';
 import { CartContextProvider } from '@/context/cart-context';

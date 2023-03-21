b3ffbdd0c21bf48701e71252294d5ab0544e4334
David Perelman <dperelman3@gmail.com>
2023-03-21 16:47:45 +0200

    Adding the "Credentials Auth Provider" & User Login Logic

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,61 +1,104 @@
-import User from '@/models/User';
+// import User from '@/models/User';
+// import NextAuth from 'next-auth';
+// import bcrypt from 'bcryptjs';
+// import Credentials from 'next-auth/providers/credentials';
+// import dbConnect from '@/config/dbConnect';
+// import CredentialsProvider from 'next-auth/providers/credentials';
+
+// import NextAuth from 'next-auth';
+
+// const { default: NextAuth } = require('next-auth');
+import { verifyPassword } from '@/lib/auth';
+import { connectToDatabase } from '@/lib/db';
 import NextAuth from 'next-auth';
-import bcrypt from 'bcryptjs';
-import Credentials from 'next-auth/providers/credentials';
-import dbConnect from '@/config/dbConnect';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import Providers from 'next-auth/providers';
 
+// export default NextAuth({
+//   session: {
+//     jwt: true,
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       async authorize(credentials, req) {
+//         dbConnect();
+//         console.log(credentials);
+
+//         const { enteredEmail, enteredPassword } = credentials;
+
+//         console.log(enteredEmail);
+//         console.log(enteredPassword);
+//         const user = await User.findOne({ enteredEmail });
+
+//         if (!user) {
+//           throw new Error('Invalid Email');
+//         }
+
+//         // const isPasswordMatched = user.comparePassword(
+//         //   enteredPassword,
+//         //   function (err, isMatch) {
+//         //     if (err) throw err;
+//         //     console.log('enteredPassword:', isMatch); // -&gt; Password123: true
+//         //   }
+//         // );
+
+//         // let passwordHash = await bcrypt.hash(user.password, salt);
+//         // const booleanResult = await bcrypt.compare(
+//         //   enteredPassword,
+//         //   passwordHash
+//         // );
+
+//         // console.log(booleanResult);
+
+//         const isPasswordMatched = await bcrypt.compare(
+//           enteredPassword,
+//           user.password
+//         );
+
+//         if (!isPasswordMatched) {
+//           throw new Error('Invalid Password');
+//         }
+
+//         return user;
+//       },
+//     }),
+//   ],
+//   //   pages: {
+//   //     signIn: '/login',
+//   //   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// });
 export default NextAuth({
-  session: {
-    jwt: true,
-  },
+  session: { jwt: true },
   providers: [
-    CredentialsProvider({
-      async authorize(credentials, req) {
-        dbConnect();
-        console.log(credentials);
+    Providers.Credentials({
+      async authorize(credentials) {
+        const client = await connectToDatabase();
 
-        const { enteredEmail, enteredPassword } = credentials;
+        const usersCollection = client.db().collection('users');
 
-        console.log(enteredEmail);
-        console.log(enteredPassword);
-        const user = await User.findOne({ enteredEmail });
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
 
         if (!user) {
-          throw new Error('Invalid Email');
+          client.close();
+          throw new Error('No user found!');
         }
 
-        // const isPasswordMatched = user.comparePassword(
-        //   enteredPassword,
-        //   function (err, isMatch) {
-        //     if (err) throw err;
-        //     console.log('enteredPassword:', isMatch); // -&gt; Password123: true
-        //   }
-        // );
-
-        // let passwordHash = await bcrypt.hash(user.password, salt);
-        // const booleanResult = await bcrypt.compare(
-        //   enteredPassword,
-        //   passwordHash
-        // );
-
-        // console.log(booleanResult);
-
-        const isPasswordMatched = await bcrypt.compare(
-          enteredPassword,
+        const isValid = await verifyPassword(
+          credentials.password,
           user.password
         );
 
-        if (!isPasswordMatched) {
-          throw new Error('Invalid Password');
+        if (!isValid) {
+          client.close();
+          throw new Error('Could not log you in!');
         }
 
-        return user;
+        client.close();
+
+        return { email: user.email };
       },
     }),
   ],
-  //   pages: {
-  //     signIn: '/login',
-  //   },
-  secret: process.env.NEXTAUTH_SECRET,
 });

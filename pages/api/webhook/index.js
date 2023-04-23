8ac6acdeb157b3850252372dd8ecaf43ae5b21a2
David Perelman <dperelman3@gmail.com>
2023-04-23 08:58:51 +0300

    micro

--- pages/api/webhook/index.js
+++ pages/api/webhook/index.js
@@ -1,5 +1,5 @@
 const { Stripe } = require('stripe');
-const { buffer } = require('micro/types/src/lib');
+const { buffer } = require('micro');
 
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET);
 

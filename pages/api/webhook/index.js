dc13f54f9e9676e26734d683235c95eed0ca6d8c
David Perelman <dperelman3@gmail.com>
2023-04-23 09:06:38 +0300

    npm install --save micro

--- pages/api/webhook/index.js
+++ pages/api/webhook/index.js
@@ -1,5 +1,5 @@
+import { buffer } from 'micro/types/src/lib';
 const { Stripe } = require('stripe');
-const { buffer } = require('micro/types/src/lib');
 
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET);
 

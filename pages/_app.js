3d61417792bd51f01a46285d11a29c1c60006c9b
David Perelman <dperelman3@gmail.com>
2023-04-30 02:00:50 +0300

    set UserOrders data table

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import '@/styles/globals.css';
 config.autoAddCss = false;
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './users/orders.css';
 
 import 'public/static/styles.css';
 
